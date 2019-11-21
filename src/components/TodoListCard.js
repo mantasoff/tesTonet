40c210b48dd2179845ea8ce71b93fb349c61ddbb
mantasoff <mantas.paskevicius1996@gmail.com>
2019-11-21 23:03:16 +0200

    Added Todo List snippets

--- src/components/TodoListCard.js
+++ src/components/TodoListCard.js
@@ -2,6 +2,25 @@ import React from 'react';
 import history from '../history';
 
 class TodoListCard extends React.Component {
+    renderSnippet = () => {
+        const filteredTodoList = this.props.TodoList.todoList.filter(todo => {
+            if(!todo.isDone) return true;
+            return false;
+        });
+
+        return filteredTodoList.map(todo => {
+            return(
+                <div className="event">
+                    <div className="content">
+                        <div className="summary">
+                            {todo.name}
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+    
     render() {
         return (
             <div className="ui link card" onClick={() => history.push(`/todo/${this.props.TodoList.id}`)}>
@@ -9,13 +28,9 @@ class TodoListCard extends React.Component {
                     <div className="header">{this.props.TodoList.name}</div>
                 </div>
                 <div className="content">
-                    <div className="ui sub header">ToDo</div>
-                    <div className="ui small feed">
-                        <div className="content">
-                            <div className="summary">
-                                This is a summary
-                            </div>
-                        </div>
+                    <div className="ui sub header">Tasks</div>
+                    <div className="ui small feed">        
+                        {this.renderSnippet()}
                     </div>
                 </div>
             </div>
